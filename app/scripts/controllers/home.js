1aebbbc268457c26a6605b2f45c9b88507b04d11
Keith D. Moore <keith@kdmooreconsulting.com>
2014-10-25 00:28:12 -0600

    Implemented findNearbyPosts and findPopularPosts
    Implemented methods on InstagramService.  Added some json dumps of the
    data on the tabbed pages.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,13 @@
 'use strict';
 angular.module('Trendicity')
 
-.controller('HomeCtrl', function ($scope) {
-  $scope.posts = [];
+.controller('HomeCtrl', function ($scope, InstagramService) {
+  InstagramService.findPopularPosts().success(function(data) {
+      $scope.popularPosts = data.data;
+  });
+
+  InstagramService.findNearbyPosts(48.858844, 2.294351).success(function(data) {
+      $scope.nearbyPosts = data.data;
+  });
+
 });
--- app/scripts/services/instagram.js
+++ app/scripts/services/instagram.js
@@ -1,5 +1,42 @@
 'use strict';
 angular.module('Trendicity')
 
-.factory('InstagramService', function() {
+.service('InstagramService', function($http) {
+
+   var client_id = "75d27c9457cd4d1abbacf80a228f4a10";
+
+   this.findPopularPosts = function() {
+       var promise =
+           $http.get("https://api.instagram.com/v1/media/popular", {
+                params: { client_id: client_id }}).
+             success(function(data, status, headers, config) {
+                console.log(angular.toJson(data, true));
+             }).
+             error(function(data, status, headers, config) {
+                 console.log("findPopularPosts returned status:"  + status);
+             });
+       return promise;
+   };
+
+   // options.distance by default is 1 Kilometer
+   this.findNearbyPosts = function(lat, lng, options) {
+        if (!options) options = {};
+
+        options.client_id = client_id;
+        options.lat = lat;
+        options.lng = lng;
+
+        var promise =
+            $http.get("https://api.instagram.com/v1/media/search", {
+                    params: options
+                }).
+                success(function(data, status, headers, config) {
+                    console.log(angular.toJson(data, true));
+                }).
+                error(function(data, status, headers, config) {
+                    console.log("findNearbyPosts returned status:"  + status);
+                });
+        return promise;
+   };
+
 });
