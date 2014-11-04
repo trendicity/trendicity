f5c0461d8f6205a867c7b12899ebd89288064abd
Robin van Baalen <robin@neverwoods.com>
2014-11-04 00:51:03 -0400

    Started on MapViewCtrl
    First basic gmaps implementation

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -1,6 +1,65 @@
 'use strict';
 angular.module('Trendicity')
 
-.controller('MapViewCtrl', function () {
+.controller('MapViewCtrl', function ($scope, $ionicLoading, $compile) {
   console.log('Inside MapViewCtrl...');
+
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+
+        console.log('INIT!');
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        $scope.map = map;
+    }
+
+        initialize();
+
+    $scope.centerOnMe = function() {
+        if(!$scope.map) {
+            return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+                $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                $scope.loading.hide();
+            }, function(error) {
+                alert('Unable to get location: ' + error.message);
+            }
+        );
+    };
+
+    $scope.clickTest = function() {
+        alert('Example of infowindow with ng-click')
+    };
 });
