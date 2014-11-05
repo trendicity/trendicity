45c8cd405e392ebef069bee9db0199717aa2fa75
Keith D. Moore <keith@kdmooreconsulting.com>
2014-11-04 23:28:22 -0600

    Fixed app hanging when API call failed. Closes #3
    Also added logic to handle the situation where multiple requests have
    been issued.

--- app/scripts/services/interceptors.js
+++ app/scripts/services/interceptors.js
@@ -4,13 +4,23 @@ angular.module('Trendicity')
 .factory('LoadingInterceptor',
   function ($injector) {
     return {
-      request: function (config) {
+      request: function(config) {
         $injector.get('$ionicLoading').show();
         return config;
       },
-      response: function (response) {
-        $injector.get('$ionicLoading').hide();
+      response: function(response) {
+        var $http = $injector.get('$http');
+        if ($http.pendingRequests.length < 1) {
+          $injector.get('$ionicLoading').hide();
+        }
         return response;
+      },
+      responseError: function(rejection) {
+        var $http = $injector.get('$http');
+        if ($http.pendingRequests.length < 1) {
+          $injector.get('$ionicLoading').hide();
+        }
+        return rejection;
       }
     };
   }
