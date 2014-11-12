ca21c384a29906570aee1b0da54cfeb444e520b6
Christopher Marshall <thoughtpalettechris@gmail.com>
2014-11-12 15:25:43 -0600

    add favorite method available, fixed issue with delete favorite

--- app/scripts/services/favorites.js
+++ app/scripts/services/favorites.js
@@ -3,11 +3,15 @@ angular.module('Trendicity')
 
 .service('FavoritesService', function($cordovaGeolocation, $http) {
 
-    var _this = this;
+    var that = this;
 
     this.add = function (location) {
-        // TODO: Figure out where to call this
-        // localStorage.setItem('');
+        var currentFavorites = JSON.parse(localStorage.getItem('Trendicity:Favorites'));
+
+        currentFavorites.push(location);
+        localStorage.setItem('Trendicity:Favorites', JSON.stringify(currentFavorites));
+
+        return this.getFavorites();
     };
 
     this.delete = function (favorite) {
@@ -15,14 +19,14 @@ angular.module('Trendicity')
 
         angular.forEach(currentFavorites, function(i){
             if(i.id === favorite.id) {
-                currentFavorites.splice(i,1);
-                return;
+                currentFavorites.splice(currentFavorites.indexOf(i), 1 );
+                return false;
             }
         });
 
         localStorage.setItem('Trendicity:Favorites', JSON.stringify(currentFavorites));
 
-        return JSON.parse(localStorage.getItem('Trendicity:Favorites'));
+        return this.getFavorites();
     };
 
     this.getFavorites = function () {
