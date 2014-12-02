b9f882699e226445b7013dd510150ef8b3998a67
antonshevchenko <antonshevchenko@icloud.com>
2014-12-02 00:02:49 -0500

    Added spacing

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -13,6 +13,7 @@ angular.module('Trendicity')
     var min = (screenWidth < imageWidth) ? screenWidth : imageWidth;
     return min + itemAvatarHeight + buttonBarHeight;
   }
+  
   $scope.itemHeight = getItemHeight();
 
   // Display action sheet
