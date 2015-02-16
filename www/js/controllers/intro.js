20bfc3aae2cd8afd44015d1bf83cbbae397d8c1d
Keith D. Moore <keith@kdmooreconsulting.com>
2015-02-15 19:33:54 -0600

    updated page 3 of about page

--- www/js/controllers/intro.js
+++ www/js/controllers/intro.js
@@ -1,13 +1,14 @@
 'use strict';
 angular.module('Trendicity')
 
-.controller('IntroCtrl', function($scope, $state, $timeout, $ionicSlideBoxDelegate, $ionicSideMenuDelegate,
-                                  $ionicHistory, localStorageService, GeolocationService) {
-
-    //** Settings some default scope variables.
-    $scope.fetchingPosition = false;
-    $scope.noPosition = false;
-
+.controller('IntroCtrl', function(
+  $scope,
+  $state,
+  $timeout,
+  $ionicSlideBoxDelegate,
+  $ionicSideMenuDelegate,
+  $ionicHistory,
+  localStorageService) {
   $scope.$on('$ionicView.beforeEnter', function() {
     // Clear history since this might have been a forced redirect
     $ionicHistory.clearHistory();
@@ -31,25 +32,12 @@ angular.module('Trendicity')
     $ionicSlideBoxDelegate.previous();
   };
 
-    $scope.getCurrentPosition = function () {
-        $scope.fetchingPosition = true;
-        GeolocationService.getCurrentPosition()
-            .then(
-                function (position) {
-                    $scope.next();
-                },
-                function (fallbackPosition) {
-                    // Show error message
-                    $scope.noPosition = true;
-                }
-            )
-            ['finally'](function () {
-                $scope.fetchingPosition = false;
-            });
-    };
-
   // Called each time the slide changes
   $scope.slideChanged = function(index) {
     $scope.slideIndex = index;
+  };
+
+  $scope.openWindow = function(url) {
+    window.open(url, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
   };
 });
