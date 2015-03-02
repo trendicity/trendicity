05eed27426848ef41ccc34bc7806cbffec803790
Robin van Baalen <robin@neverwoods.com>
2015-03-02 00:23:56 -0400

    Fixes #63
    - Reverted back to 5436cd5a6cfb5c8b4e9d81041c962873b5600a53
    - Added .ng-hide { display: none }

--- www/js/controllers/intro.js
+++ www/js/controllers/intro.js
@@ -1,7 +1,6 @@
 'use strict';
 angular.module('Trendicity')
 
-.constant('BUTTON_ACTION', {'BACK': 0, 'NEXT': 1})
 .controller('IntroCtrl', function(
   $scope,
   $state,
@@ -9,8 +8,7 @@ angular.module('Trendicity')
   $ionicSlideBoxDelegate,
   $ionicSideMenuDelegate,
   $ionicHistory,
-  localStorageService,
-  BUTTON_ACTION) {
+  localStorageService) {
   $scope.$on('$ionicView.beforeEnter', function() {
     // Clear history since this might have been a forced redirect
     $ionicHistory.clearHistory();
@@ -21,60 +19,16 @@ angular.module('Trendicity')
   // Mark intro as seen
   localStorageService.set('seenIntro', true);
 
-  // Default button labels for the first slideIndex
-  $scope.backButtonLabel = 'Skip';
-  $scope.nextButtonLabel = 'Next';
-
-  // Set the slideIndex on 0 by default to prevent 'undefined' value.
-  $scope.slideIndex = 0;
-
   // Called to navigate to the main app
-  var startApp = function() {
+  $scope.startApp = function() {
     $state.go('app.home.map');
   };
 
-  var updateButtonLabel = function (slideIndex, buttonAction) {
-    switch (slideIndex) {
-      case 0:
-        if (buttonAction === BUTTON_ACTION.BACK) {
-          // Skip was clicked
-          startApp();
-        } else {
-          $scope.backButtonLabel = 'Back';
-          $scope.nextButtonLabel = 'Next';
-        }
-        break;
-      case 1:
-        if (buttonAction === BUTTON_ACTION.BACK) {
-          $scope.backButtonLabel = 'Skip';
-          $scope.nextButtonLabel = 'Next';
-        } else {
-          $scope.backButtonLabel = 'Back';
-          $scope.nextButtonLabel = 'Explore';
-        }
-
-        break;
-      case 2:
-        if (buttonAction === BUTTON_ACTION.BACK) {
-          $scope.backButtonLabel = 'Back';
-          $scope.nextButtonLabel = 'Next';
-        } else {
-          // 'Explore' was clicked.
-          startApp();
-        }
-        break;
-    }
-  };
-
-  $scope.next = function(slideIndex) {
-    updateButtonLabel(slideIndex, BUTTON_ACTION.NEXT);
-
+  $scope.next = function() {
     $ionicSlideBoxDelegate.next();
   };
 
-  $scope.previous = function(slideIndex) {
-    updateButtonLabel(slideIndex, BUTTON_ACTION.BACK);
-
+  $scope.previous = function() {
     $ionicSlideBoxDelegate.previous();
   };
 
