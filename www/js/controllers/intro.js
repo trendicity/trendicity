f30983e62cf9a50e704c51c727edbfb986056357
Robin van Baalen <robin@neverwoods.com>
2015-03-01 22:39:56 -0400

    Fixes #63
    Refactored navigation buttons to a single button for back and a single
    button for next action. Let the controller do the thinking.

--- www/js/controllers/intro.js
+++ www/js/controllers/intro.js
@@ -1,6 +1,7 @@
 'use strict';
 angular.module('Trendicity')
 
+.constant('BUTTON_ACTION', {'BACK': 0, 'NEXT': 1})
 .controller('IntroCtrl', function(
   $scope,
   $state,
@@ -8,7 +9,8 @@ angular.module('Trendicity')
   $ionicSlideBoxDelegate,
   $ionicSideMenuDelegate,
   $ionicHistory,
-  localStorageService) {
+  localStorageService,
+  BUTTON_ACTION) {
   $scope.$on('$ionicView.beforeEnter', function() {
     // Clear history since this might have been a forced redirect
     $ionicHistory.clearHistory();
@@ -19,16 +21,61 @@ angular.module('Trendicity')
   // Mark intro as seen
   localStorageService.set('seenIntro', true);
 
+  // Default button labels for the first slideIndex
+  $scope.backButtonLabel = 'Skip';
+  $scope.nextButtonLabel = 'Next';
+
+  // Set the slideIndex on 0 by default to prevent 'undefined' value.
+  $scope.slideIndex = 0;
+
   // Called to navigate to the main app
-  $scope.startApp = function() {
+  var startApp = function() {
     $state.go('app.home.map');
   };
 
-  $scope.next = function() {
+  var updateButtonLabel = function (slideIndex, buttonAction) {
+    switch (slideIndex) {
+      case 0:
+        if (buttonAction === BUTTON_ACTION.BACK) {
+          // Skip was clicked
+          startApp();
+        } else {
+          console.log('nextback');
+          $scope.backButtonLabel = 'Back';
+          $scope.nextButtonLabel = 'Next';
+        }
+        break;
+      case 1:
+        if (buttonAction === BUTTON_ACTION.BACK) {
+          $scope.backButtonLabel = 'Skip';
+          $scope.nextButtonLabel = 'Next';
+        } else {
+          $scope.backButtonLabel = 'Back';
+          $scope.nextButtonLabel = 'Explore';
+        }
+
+        break;
+      case 2:
+        if (buttonAction === BUTTON_ACTION.BACK) {
+          $scope.backButtonLabel = 'Back';
+          $scope.nextButtonLabel = 'Explore';
+        } else {
+          // 'Explore' was clicked.
+          startApp();
+        }
+        break;
+    }
+  };
+
+  $scope.next = function(slideIndex) {
+    updateButtonLabel(slideIndex, BUTTON_ACTION.NEXT);
+
     $ionicSlideBoxDelegate.next();
   };
 
-  $scope.previous = function() {
+  $scope.previous = function(slideIndex) {
+    updateButtonLabel(slideIndex, BUTTON_ACTION.BACK);
+
     $ionicSlideBoxDelegate.previous();
   };
 
